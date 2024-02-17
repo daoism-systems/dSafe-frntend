64f4607b266aaf0b7db059860ff01e9556b8e1db
Angel Lakra <angel.lakra@chainlabs.in>
2024-02-17 20:15:11 +0530

    View Delegates page components added

--- src/FakeData.ts
+++ src/FakeData.ts
@@ -1,61 +1,65 @@
-
 export const fakeDelegateData = [
-    {
-      "safeAddress": "0xSafeAddress1",
-      "delegates": [
-        {"delegate": "0xabcd", "delegator": "0x1234"},
-        {"delegate": "0xef01", "delegator": "0x5678"}
-      ]
-    },
-    {
-      "safeAddress": "0xSafeAddress2",
-      "delegates": [
-        {"delegate": "0x1234", "delegator": "0x9abc"},
-        {"delegate": "0x5678", "delegator": "0xdef0"}
-      ]
-    }
-  ];
-  
-  export const fakeSafeOwnerData = [
-    { owner: "0xabcd", safeAddress: "0x1234", threshold: 3 },
-    { owner: "0xbcde", safeAddress: "0x2345", threshold: 2 },
-    { owner: "0xcdef", safeAddress: "0x3456", threshold: 4 },
-    { owner: "0xdef0", safeAddress: "0x4567", threshold: 1 },
-    { owner: "0xef01", safeAddress: "0x5678", threshold: 5 },
-  ];
+  {
+    safeAddress: '0xSafeAddress1',
+    delegates: [
+      { delegate: '0xabcd', delegator: '0x1234' },
+      { delegate: '0xef01', delegator: '0x5678' },
+    ],
+  },
+  {
+    safeAddress: '0xSafeAddress2',
+    delegates: [
+      { delegate: '0x1234', delegator: '0x9abc' },
+      { delegate: '0x5678', delegator: '0xdef0' },
+    ],
+  },
+]
 
-  export const fakeSafeDetails = [
-    {
-      address: '0xSafeAddress1',
-      nonce: 0,
-      threshold: 2,
-      owners: ['0xOwner1', '0xOwner2'],
-      masterCopy: '0xMasterCopy1',
-      modules: ['0xModule1', '0xModule2'],
-      fallbackHandler: '0xFallbackHandler1',
-      guard: '0xGuard1',
-      version: '1.0.1',
-    },
-    {
-      address: '0xSafeAddress2',
-      nonce: 1,
-      threshold: 3,
-      owners: ['0xOwner3', '0xOwner4', '0xOwner5'],
-      masterCopy: '0xMasterCopy2',
-      modules: ['0xModule3'],
-      fallbackHandler: '0xFallbackHandler2',
-      guard: '0xGuard2',
-      version: '1.2.0',
-    },
-    {
-      address: '0xSafeAddress3',
-      nonce: 2,
-      threshold: 1,
-      owners: ['0xOwner6'],
-      masterCopy: '0xMasterCopy3',
-      modules: ['0xModule4', '0xModule5', '0xModule6'],
-      fallbackHandler: '0xFallbackHandler3',
-      guard: '0xGuard3',
-      version: '2.0.0',
-    },
-  ];
+export const fakeSafesOfOwner = [
+  '0x493132b20De7b3e7825238414c204f8f30b17C7D',
+  '0x6fa4ef12fA2Fea9e4eBD7E46C0Ffff8bfEF1a0F8',
+]
+
+export const fakeSafeOwnerData = [
+  { owner: '0xabcd', safeAddress: '0x1234', threshold: 3 },
+  { owner: '0xbcde', safeAddress: '0x2345', threshold: 2 },
+  { owner: '0xcdef', safeAddress: '0x3456', threshold: 4 },
+  { owner: '0xdef0', safeAddress: '0x4567', threshold: 1 },
+  { owner: '0xef01', safeAddress: '0x5678', threshold: 5 },
+]
+
+export const fakeSafeDetails = [
+  {
+    address: '0xSafeAddress1',
+    nonce: 0,
+    threshold: 2,
+    owners: ['0xOwner1', '0xOwner2'],
+    masterCopy: '0xMasterCopy1',
+    modules: ['0xModule1', '0xModule2'],
+    fallbackHandler: '0xFallbackHandler1',
+    guard: '0xGuard1',
+    version: '1.0.1',
+  },
+  {
+    address: '0xSafeAddress2',
+    nonce: 1,
+    threshold: 3,
+    owners: ['0xOwner3', '0xOwner4', '0xOwner5'],
+    masterCopy: '0xMasterCopy2',
+    modules: ['0xModule3'],
+    fallbackHandler: '0xFallbackHandler2',
+    guard: '0xGuard2',
+    version: '1.2.0',
+  },
+  {
+    address: '0xSafeAddress3',
+    nonce: 2,
+    threshold: 1,
+    owners: ['0xOwner6'],
+    masterCopy: '0xMasterCopy3',
+    modules: ['0xModule4', '0xModule5', '0xModule6'],
+    fallbackHandler: '0xFallbackHandler3',
+    guard: '0xGuard3',
+    version: '2.0.0',
+  },
+]
--- src/components/ViewDelegates.tsx
+++ src/components/ViewDelegates.tsx
@@ -1,63 +1,82 @@
-import React, { useState } from 'react';
-import { fakeDelegateData } from '../FakeData'
+import React, { useEffect, useState } from 'react'
+import {
+  fakeDelegateData,
+  fakeSafeDetails,
+  fakeSafesOfOwner,
+} from '../FakeData'
+
+import { getSafeSingletonDeployment } from '@safe-global/safe-deployments'
+import AnimatedInput from './AnimatedInput'
+import { Label, Select } from 'flowbite-react'
 
 interface DelegateInfo {
-  delegate: string;
-  delegator: string;
+  delegate: string
+  delegator: string
 }
 
 interface SafeAddressData {
-  safeAddress: string;
-  delegates: DelegateInfo[];
+  safeAddress: string
+  delegates: DelegateInfo[]
 }
 
 interface SelectSafeProps {
-  data: SafeAddressData[];
+  data: SafeAddressData[]
 }
 
 const ViewDelegates: React.FC = () => {
-  const [selectedSafeAddress, setSelectedSafeAddress] = useState<string>('');
-  const [data, setData] = useState(fakeDelegateData);
+  const [selectedSafeAddress, setSelectedSafeAddress] = useState<string>('')
+  const [safes, setSafes] = useState<string[]>([])
+  const [data, setData] = useState(fakeDelegateData)
+
+  const safeAbi = getSafeSingletonDeployment()?.abi
+
+  useEffect(() => {
+    // TODO: Fetch safes from dSafe here
+    setSafes(fakeSafesOfOwner)
+  }, [])
 
   const handleSafeAddressChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedSafeAddress(e.target.value);
-  };
+    setSelectedSafeAddress(e.target.value)
+  }
 
   const selectedDelegates = data.find(
-    (d) => d.safeAddress === selectedSafeAddress
-  )?.delegates;
+    (d) => d.safeAddress === selectedSafeAddress,
+  )?.delegates
 
   return (
     <div>
       <div>
-        <label htmlFor="safeSelect">Select SAFE</label>
-        <select id="safeSelect" onChange={handleSafeAddressChange} value={selectedSafeAddress}>
-          <option value="">Select a Safe Address</option>
-          {data.map((item) => (
-            <option key={item.safeAddress} value={item.safeAddress}>
-              {item.safeAddress}
-            </option>
-          ))}
-        </select>
+        <h2 className="text-black text-2xl font-bold  text-left">
+          View Delegates
+        </h2>
+        <div className="flex max-w-md flex-col gap-4">
+          <div className="flex flex-col gap-4 mt-5">
+            <div>
+              <Label
+                htmlFor="safeSelect"
+                className={`absolute left-2 transition-all duration-300 ease-in-out top-[-12px] text-sm text-gray-700 bg-white px-1 text-black`}
+              >
+                Select Safe
+              </Label>
+              <select
+                id="safeSelect"
+                onChange={handleSafeAddressChange}
+                value={selectedSafeAddress}
+                className={`w-full text-sm placeholder:text-sm p-4 border  rounded focus:border-gray-700 outline-none`}
+              >
+                <option value="">Select a Safe Address</option>
+                {safes.map((safe) => (
+                  <option key={safe} value={safe}>
+                    {safe}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Delegate</th>
-            <th>Delegator</th>
-          </tr>
-        </thead>
-        <tbody>
-          {selectedDelegates?.map((delegate, index) => (
-            <tr key={index}>
-              <td>{delegate.delegate}</td>
-              <td>{delegate.delegator}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
-  );
-};
+  )
+}
 
-export default ViewDelegates;
+export default ViewDelegates
