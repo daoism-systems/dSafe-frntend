4933769fdafc1de312568c9f68e06412267e36ac
Angel Lakra <angel.lakra@chainlabs.in>
2024-02-17 16:06:06 +0530

    add delegates section added

--- src/components/AddDelegate.tsx
+++ src/components/AddDelegate.tsx
@@ -1,68 +1,70 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, {
+  useState,
+  ChangeEvent,
+  FormEvent,
+  FormEventHandler,
+} from 'react'
+
+import { Button, Label, TextInput } from 'flowbite-react'
+import AnimatedInput from './AnimatedInput'
 
 const AddDelegate: React.FC = () => {
-  const [delegateAddress, setDelegateAddress] = useState<string>('');
-  const [safeAddress, setSafeAddress] = useState<string>('');
+  const [delegateAddress, setDelegateAddress] = useState<string>('')
+  const [safeAddress, setSafeAddress] = useState<string>('')
 
   const handleDelegateChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+    const value = e.target.value
     // Allow only hexadecimal characters and limit length to 42 including the '0x'
-      setDelegateAddress(value);
-  };
+    setDelegateAddress(value)
+  }
 
   const handleSafeChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+    const value = e.target.value
     // Allow only hexadecimal characters and limit length to 42 including the '0x'
-      setSafeAddress(value);
-  };
+    setSafeAddress(value)
+  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
     // Check if both addresses are exactly 42 characters long
     if (delegateAddress.length === 42 && safeAddress.length === 42) {
       // Perform submit actions here, such as sending data to an API
-      console.log('Delegate Address:', delegateAddress);
-      console.log('Safe Address:', safeAddress);
+      console.log('Delegate Address:', delegateAddress)
+      console.log('Safe Address:', safeAddress)
     } else {
       // If input lengths are invalid, handle the error accordingly
-      console.error('Both addresses must be exactly 42 characters long.');
+      console.error('Both addresses must be exactly 42 characters long.')
     }
-  };
+  }
 
   return (
     <div>
-      <h2 className='text-black text-2xl font-bold'>Add Delegate</h2>
-    <form onSubmit={handleSubmit}>
-      <div>
-      <label className="form-control w-full max-w-xs">
-  <div className="label">
-    <span className="label-text">What is your name?</span>
-    <span className="label-text-alt">Top Right label</span>
-  </div>
-  <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-  <div className="label">
-    <span className="label-text-alt">Bottom Left label</span>
-    <span className="label-text-alt">Bottom Right label</span>
-  </div>
-</label>
-      </div>
-      <div>
-        <label htmlFor="safeAddress">Safe Address:</label>
-        <input
-          type="text"
-          id="safeAddress"
-          value={safeAddress}
-          onChange={handleSafeChange}
-          maxLength={42}
-          placeholder="0x456..."
+      <h2 className="text-black text-2xl font-bold  text-left">Add Delegate</h2>
+      <form className="flex max-w-md flex-col gap-4" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4 mt-5">
+          <AnimatedInput
+            value={safeAddress}
+            setValue={setSafeAddress}
+            placeholder="Safe Address"
+            pattern="^0x[a-fA-F0-9]{40}$"
           />
-      </div>
-      <button type="submit" disabled={delegateAddress.length !== 42 || safeAddress.length !== 42}>
-        Add Delegate
-      </button>
-    </form>
-          </div>
-  );
-};
+          <AnimatedInput
+            value={delegateAddress}
+            setValue={setDelegateAddress}
+            placeholder="Delegate Address"
+            pattern="^0x[a-fA-F0-9]{40}$"
+          />
+        </div>
+        <Button
+          type="submit"
+          disabled={delegateAddress.length !== 42 || safeAddress.length !== 42}
+          className="bg-teal-500 text-white inline-block hover:bg-teal-600 cursor-pointer disabled:cursor-not-allowed"
+        >
+          Add Delegate
+        </Button>
+      </form>
+    </div>
+  )
+}
 
-export default AddDelegate;
+export default AddDelegate
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+import flowbite from 'flowbite/plugin'
 export default {
   purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -8,5 +10,8 @@ export default {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite
+  ],
+  content: ["node_modules/flowbite-react/lib/esm/**/*.js"]
 }
